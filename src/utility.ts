ef796662a8161c491cd804b17f750d977b9d97ea
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-23 17:15:58 +0000

    Boilerplate code
    Ran Bro Code from the vscode docs to generate a new extension.

---
+++ src/utility.ts
@@ -0,0 +1,36 @@
+"use strict";
+import * as vscode from "vscode";
+import { Constants } from "./constants";
+
+export class Utility {
+    public static async getPythonPath(document: vscode.TextDocument): Promise<string> {
+        try {
+            const extension = vscode.extensions.getExtension("ms-python.python");
+            if (!extension) {
+                return Constants.python;
+            }
+            const usingNewInterpreterStorage = extension.packageJSON?.featureFlags?.usingNewInterpreterStorage;
+            if (usingNewInterpreterStorage) {
+                if (!extension.isActive) {
+                    await extension.activate();
+                }
+                const execCommand = extension.exports.settings.getExecutionDetails ?
+                    extension.exports.settings.getExecutionDetails(document?.uri).execCommand :
+                    extension.exports.settings.getExecutionCommand(document?.uri);
+                return execCommand ? execCommand.join(" ") : Constants.python;
+            } else {
+                return this.getConfiguration("python", document).get<string>("pythonPath");
+            }
+        } catch (error) {
+            return Constants.python;
+        }
+    }
+
+    public static getConfiguration(section?: string, document?: vscode.TextDocument): vscode.WorkspaceConfiguration {
+        if (document) {
+            return vscode.workspace.getConfiguration(section, document.uri);
+        } else {
+            return vscode.workspace.getConfiguration(section);
+        }
+    }
+}
