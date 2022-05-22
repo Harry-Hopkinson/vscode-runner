76ef65249f3529669e05ff961599ece969b14963
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-05-22 21:05:27 +0100

    Lint and migrate to yarn

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 "use strict";
 
 export class Constants {
-    public static readonly python = "python";
+  public static readonly python = "python";
 }
--- src/extension.ts
+++ src/extension.ts
@@ -3,35 +3,42 @@ import * as vscode from "vscode";
 import { CodeManager } from "./codeManager";
 
 export function activate(context: vscode.ExtensionContext) {
+  const codeManager = new CodeManager();
 
-    const codeManager = new CodeManager();
+  vscode.window.onDidCloseTerminal(() => {
+    codeManager.onDidCloseTerminal();
+  });
 
-    vscode.window.onDidCloseTerminal(() => {
-        codeManager.onDidCloseTerminal();
-    });
+  const run = vscode.commands.registerCommand(
+    "code-runner.run",
+    (fileUri: vscode.Uri) => {
+      codeManager.run(null, fileUri);
+    }
+  );
 
-    const run = vscode.commands.registerCommand("code-runner.run", (fileUri: vscode.Uri) => {
-        codeManager.run(null, fileUri);
-    });
+  const runCustomCommand = vscode.commands.registerCommand(
+    "code-runner.runCustomCommand",
+    () => {
+      codeManager.runCustomCommand();
+    }
+  );
 
-    const runCustomCommand = vscode.commands.registerCommand("code-runner.runCustomCommand", () => {
-        codeManager.runCustomCommand();
-    });
+  const runByLanguage = vscode.commands.registerCommand(
+    "code-runner.runByLanguage",
+    () => {
+      codeManager.runByLanguage();
+    }
+  );
 
-    const runByLanguage = vscode.commands.registerCommand("code-runner.runByLanguage", () => {
-        codeManager.runByLanguage();
-    });
+  const stop = vscode.commands.registerCommand("code-runner.stop", () => {
+    codeManager.stop();
+  });
 
-    const stop = vscode.commands.registerCommand("code-runner.stop", () => {
-        codeManager.stop();
-    });
-
-    context.subscriptions.push(run);
-    context.subscriptions.push(runCustomCommand);
-    context.subscriptions.push(runByLanguage);
-    context.subscriptions.push(stop);
-    context.subscriptions.push(codeManager);
+  context.subscriptions.push(run);
+  context.subscriptions.push(runCustomCommand);
+  context.subscriptions.push(runByLanguage);
+  context.subscriptions.push(stop);
+  context.subscriptions.push(codeManager);
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
